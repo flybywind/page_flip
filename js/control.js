22332fb5393e6bb7f3b6c1056850639396b0bcd5
flybywind@foxmail.com <flybywind@foxmail.com>
2015-09-30 14:13:52 +0800

    finish effect

--- js/control.js
+++ js/control.js
@@ -3,10 +3,11 @@ $(function(){
   book.flip().init({
     height: 600,
     width: 1000,
-  	effect_time: 2000,  // 2s
+  	effect_time: 4000,  // unit ms
     vh: 5500,
     margin: "33px auto",
-	effect: book.flipEffect.linear(),
+	effect: book.flipEffect.easeInOut(2000),
+	//effect: book.flipEffect.linear(),
   });
 
   $(".next_btn").on("click", function() {
